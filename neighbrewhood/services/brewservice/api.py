776b3c9823a3af41a1d2c612ad915769c25b6c3e
John Bridstrup <john@advanced.farm>
2023-12-03 08:36:13 -0800

    name the urls

--- neighbrewhood/services/brewservice/api.py
+++ neighbrewhood/services/brewservice/api.py
@@ -11,29 +11,54 @@ from .schemas import BrewCreateSchema, BrewResponseSchema, BrewTypeSchema, Quali
 
 brew_router = Router(tags=["Brewers", "Brews"])
 
-@brew_router.get("brewtypes", auth=JWTAuth(), response=List[BrewTypeSchema])
+@brew_router.get(
+    "brewtypes", 
+    auth=JWTAuth(), 
+    response=List[BrewTypeSchema],
+    url_name="brew_brew_types",
+)
 @paginate
 def brew_types(request):
     return BrewType.objects.all()
 
-@brew_router.get("qualities", auth=JWTAuth(), response=List[QualitySchema])
+@brew_router.get(
+    "qualities", 
+    auth=JWTAuth(), 
+    response=List[QualitySchema],
+    url_name="brew_qualities",
+)
 @paginate
 def qualities(request):
     return Quality.objects.all()
 
-@brew_router.get("brews", auth=JWTAuth(), response=List[BrewResponseSchema])
+@brew_router.get(
+    "brews", 
+    auth=JWTAuth(), 
+    response=List[BrewResponseSchema],
+    url_name="brew_brews",
+)
 @paginate
 def brews(request):
     return Brew.objects.all()
 
-@brew_router.get("brews/{id}", auth=JWTAuth(), response={200: BrewResponseSchema, codes_4xx: DefaultError})
+@brew_router.get(
+    "brews/{id}", 
+    auth=JWTAuth(), 
+    response={200: BrewResponseSchema, codes_4xx: DefaultError},
+    url_name="brew_brew_detail",
+)
 def brew(request, id_):
     try:
         return Brew.objects.get(id=id_)
     except Brew.DoesNotExist:
         return 404, {'detail': f'Brew with id {id_} does not exist'}
 
-@brew_router.post("createBrew", auth=JWTAuth(), response=BrewResponseSchema)
+@brew_router.post(
+    "createBrew", 
+    auth=JWTAuth(), 
+    response=BrewResponseSchema,
+    url_name="brew_create_brew",
+)
 def create_brew(request, brew: BrewCreateSchema):
     if not getattr(request.user, "brewer", True):
         return 400, {'detail': "You have not created a profile"}
--- neighbrewhood/services/users/api.py
+++ neighbrewhood/services/users/api.py
@@ -13,11 +13,20 @@ from .schemas import BrewerResponseSchema, CreateBrewerSchema, UserSchema, UserR
 users_router = Router(tags=["Users", "Brewers"])
 User = get_user_model()
 
-@users_router.get("me", auth=JWTAuth(), response={200: UserSchema, codes_4xx: DefaultError})
+@users_router.get(
+    "me", 
+    auth=JWTAuth(), 
+    response={200: UserSchema, codes_4xx: DefaultError},
+    url_name="users_me"
+)
 def me(request):
     return request.user
 
-@users_router.post("register", response={201: UserSchema, codes_4xx: DefaultError})
+@users_router.post(
+    "register", 
+    response={201: UserSchema, codes_4xx: DefaultError},
+    url_name="users_register",
+)
 def register(request, user_details: UserRegisterSchema):
     try:
         new_user = User.objects.create_user(
@@ -33,7 +42,12 @@ def register(request, user_details: UserRegisterSchema):
         return 400, {"detail": str(e)}
     return 201, new_user
 
-@users_router.post("createBrewerProfile", auth=JWTAuth(), response={201: BrewerResponseSchema, codes_4xx: DefaultError})
+@users_router.post(
+    "createBrewerProfile", 
+    auth=JWTAuth(), 
+    response={201: BrewerResponseSchema, codes_4xx: DefaultError},
+    url_name="users_create_brewer",
+)
 def create_brewer(request, brewer: CreateBrewerSchema):
     if getattr(request.user, "brewer", False):
         return 400, {'detail': "You already have a brewer profile."}
@@ -50,7 +64,12 @@ def create_brewer(request, brewer: CreateBrewerSchema):
 
     return 201, brewer_obj
 
-@users_router.get("profile", auth=JWTAuth(), response={200: BrewerResponseSchema, codes_4xx: DefaultError})
+@users_router.get(
+    "profile", 
+    auth=JWTAuth(), 
+    response={200: BrewerResponseSchema, codes_4xx: DefaultError},
+    url_name="users_profile"
+)
 def brewer_profile(request):
     if not getattr(request.user, "brewer", True):
         return 400, {'detail': "You have not created a profile"}
